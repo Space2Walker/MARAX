a83dc4bd080d91456a5d4632cd91593d5aa79aaf
Space2Walker <21970520+Space2Walker@users.noreply.github.com>
2019-07-08 00:51:40 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,3 +6,6 @@ wget https://raw.githubusercontent.com/Space2Walker/MARAX/master/ignition.json
 
 #make install-script executable
 chmod +x coreos-install 
+
+#install CoreOS
+./coreos-install -d /dev/sda -i ./ignition.json
