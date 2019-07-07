bc8cfacd4f4ea5a577b2a1907e3cda18d690e844
Space2Walker <21970520+Space2Walker@users.noreply.github.com>
2019-07-08 00:14:16 +0200

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# get latest install-script and config
+wget https://raw.githubusercontent.com/coreos/init/master/bin/coreos-install
+wget https://raw.githubusercontent.com/Space2Walker/MARAX/master/ignition.json
+
+#make install-script executable
+chmod +x coreos-install 
